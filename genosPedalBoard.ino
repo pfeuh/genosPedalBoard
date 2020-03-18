1e2f69006a393a21485e6c3b301f6bf44bd71ead
pfeuh <ze.pfeuh@gmail.com>
2020-03-18 10:54:34 +0100

    First working version communicating with genos

--- genosPedalBoard.ino
+++ genosPedalBoard.ino
@@ -7,25 +7,47 @@
 #define MIDI_CHANNEL 0
 #define MIDI_NOTEOFF 0x80
 #define MIDI_NOTEON 0x90
+#define MIDI_CC 0xb0
 #define MIDI_DEFAULT_VELOCITY 0x7f
+#define MIDI_CC_VALUE_OFF 0
+#define MIDI_CC_VALUE_ON 127
 
-#define GENOS_FINGERED_ON_BASS_NOTE 42
+#define GENOS_FINGERED_ON_BASS_CC 34
+#define SUSTAIN_CC 1
+#define PORTAMENTO_CC 2
+
+/* custom config */
+#define PEDAL_1_CC GENOS_FINGERED_ON_BASS_CC
+#define PEDAL_2_CC SUSTAIN_CC
+#define PEDAL_3_CC PORTAMENTO_CC
 
 #define MIDI_OUT_DRV Serial
 #define CONSOLE_OUT_DEV Serial
-#define DEBUG_ON_UART
+//~ #define DEBUG_ON_UART
 
-GPIO_IN pedal1     = GPIO_IN(2, GPIO_IN_LOGIC_AUTO);
-GPIO_IN pedal2     = GPIO_IN(3, GPIO_IN_LOGIC_AUTO);
-GPIO_IN pedal3     = GPIO_IN(4, GPIO_IN_LOGIC_AUTO);
-GPIO_IN pedalSlow  = GPIO_IN(5);
-GPIO_IN pedalFast  = GPIO_IN(6);
-GPIO_OUT relay1    = GPIO_OUT(7, GPIO_OUT_LOGIC_REVERTED);
+GPIO_IN pedal_1      = GPIO_IN(2, GPIO_IN_LOGIC_AUTO);
+GPIO_IN pedal_2      = GPIO_IN(3, GPIO_IN_LOGIC_AUTO);
+GPIO_IN pedal_3      = GPIO_IN(4, GPIO_IN_LOGIC_AUTO);
+GPIO_IN unusedPedal  = GPIO_IN(5);
+GPIO_IN sustainPedal = GPIO_IN(6);
+GPIO_OUT relay1      = GPIO_OUT(7, GPIO_OUT_LOGIC_REVERTED);
 
 const char PROGMEM title[]   = "genosPedalBoard   v. " GENOS_GPIO_IN_BOARD_VERSION;
 const char PROGMEM splash1[] = "Pedal             v. " GPIO_IN_VERSION;
 const char PROGMEM splash2[] = "Output            v. " GPIO_OUT_VERSION;
 const char PROGMEM timestamp[] = __DATE__ " " __TIME__;
+
+void cPrint(const char* address)
+{
+    while(pgm_read_byte(address))
+        CONSOLE_OUT_DEV.write(pgm_read_byte(address++));
+}
+
+void cPrintLn(const char* address)
+{
+    cPrint(address);
+    CONSOLE_OUT_DEV.write('\n');
+}
 
 void noteOn(byte note)
 {
@@ -36,7 +58,7 @@ void noteOn(byte note)
     CONSOLE_OUT_DEV.write(' ');
     CONSOLE_OUT_DEV.println(MIDI_DEFAULT_VELOCITY, HEX);
     #else
-    MIDI_OUT_DRV.write(MIDI_NOTEON | GPIO_IN_BOARD_MIDI_CHANNEL);
+    MIDI_OUT_DRV.write(MIDI_NOTEON | MIDI_CHANNEL);
     MIDI_OUT_DRV.write(note);
     MIDI_OUT_DRV.write(MIDI_DEFAULT_VELOCITY);
     #endif
@@ -51,92 +73,81 @@ void noteOff(byte note)
     CONSOLE_OUT_DEV.write(' ');
     CONSOLE_OUT_DEV.println(0);
     #else
-    MIDI_OUT_DRV.write(MIDI_NOTEON | GPIO_IN_BOARD_MIDI_CHANNEL);
+    MIDI_OUT_DRV.write(MIDI_NOTEOFF | MIDI_CHANNEL);
     MIDI_OUT_DRV.write(note);
     MIDI_OUT_DRV.write(MIDI_DEFAULT_VELOCITY);
     #endif
 }
 
-void pedal1On()
+void controlChange(byte index, byte value)
 {
     #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 1 ON"));
+    CONSOLE_OUT_DEV.print(MIDI_CC | MIDI_CHANNEL, HEX);
+    CONSOLE_OUT_DEV.write(' ');
+    CONSOLE_OUT_DEV.print(index, HEX);
+    CONSOLE_OUT_DEV.write(' ');
+    CONSOLE_OUT_DEV.println(value, HEX);
+    #else
+    MIDI_OUT_DRV.write(MIDI_CC | MIDI_CHANNEL);
+    MIDI_OUT_DRV.write(index);
+    MIDI_OUT_DRV.write(value);
     #endif
 }
 
-void pedal2On()
+void pedal1_On()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 2 ON"));
-    #endif
+    controlChange(PEDAL_1_CC, MIDI_CC_VALUE_ON);
+    controlChange(PEDAL_1_CC, MIDI_CC_VALUE_OFF);
 }
 
-void pedal3On()
+void pedal2_On()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 3 ON"));
-    #endif
+    controlChange(PEDAL_2_CC, MIDI_CC_VALUE_ON);
+    //~ relay1.on();
 }
 
-void pedalSlowOn()
+void pedal3_On()
+{
+    controlChange(PEDAL_3_CC, MIDI_CC_VALUE_ON);
+}
+
+void unusedPedalOn()
 {
     #ifdef DEBUG_ON_UART
     CONSOLE_OUT_DEV.println(F("pedal slow ON"));
     #endif
 }
 
-void pedalFastOn()
+void sustainPedalOn()
 {
     #ifdef DEBUG_ON_UART
     CONSOLE_OUT_DEV.println(F("pedal fast ON"));
     #endif
 }
 
-void pedal1Off()
+void pedal1_Off()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 1 OFF"));
-    #endif
+    controlChange(PEDAL_1_CC, MIDI_CC_VALUE_ON);
+    controlChange(PEDAL_1_CC, MIDI_CC_VALUE_OFF);
 }
 
-void pedal2Off()
+void pedal2_Off()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 2 OFF"));
-    #endif
+    controlChange(PEDAL_2_CC, MIDI_CC_VALUE_OFF);
+    //~ relay1.off();
 }
 
-void pedal3Off()
+void pedal3_Off()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal 3 OFF"));
-    #endif
+    controlChange(PEDAL_3_CC, MIDI_CC_VALUE_OFF);
 }
 
-void pedalSlowOff()
+void unusedPedalOff()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal slow OFF"));
-    #endif
 }
 
-void pedalFastOff()
+void sustainPedalOff()
 {
-    #ifdef DEBUG_ON_UART
-    CONSOLE_OUT_DEV.println(F("pedal fast OFF"));
-    #endif
-}
-
-void cPrint(const char* address)
-{
-    while(pgm_read_byte(address))
-        CONSOLE_OUT_DEV.write(pgm_read_byte(address++));
-}
-
-void cPrintLn(const char* address)
-{
-    cPrint(address);
-    CONSOLE_OUT_DEV.write('\n');
 }
 
 void setup()
@@ -151,27 +162,27 @@ void setup()
     CONSOLE_OUT_DEV.begin(31250);
     #endif
 
-    pedal1.setPushedHandler(&pedal1On);
-    pedal1.setReleasedHandler(&pedal1Off);
-    pedal2.setPushedHandler(&pedal2On);
-    pedal2.setReleasedHandler(&pedal2Off);
-    pedal3.setPushedHandler(&pedal3On);
-    pedal3.setReleasedHandler(&pedal3Off);
-    pedalSlow.setPushedHandler(&pedalSlowOn);
-    pedalSlow.setReleasedHandler(&pedalSlowOff);
-    pedalFast.setPushedHandler(&pedalFastOn);
-    pedalFast.setReleasedHandler(&pedalFastOff);
+    pedal_1.setPushedHandler(&pedal1_On);
+    pedal_1.setReleasedHandler(&pedal1_Off);
+    pedal_2.setPushedHandler(&pedal2_On);
+    pedal_2.setReleasedHandler(&pedal2_Off);
+    pedal_3.setPushedHandler(&pedal3_On);
+    pedal_3.setReleasedHandler(&pedal3_Off);
+    unusedPedal.setPushedHandler(&unusedPedalOn);
+    unusedPedal.setReleasedHandler(&unusedPedalOff);
+    sustainPedal.setPushedHandler(&sustainPedalOn);
+    sustainPedal.setReleasedHandler(&sustainPedalOff);
     
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-    pedal1.sequencer();
-    pedal2.sequencer();
-    pedal3.sequencer();
-    pedalSlow.sequencer();
-    pedalFast.sequencer();
+    pedal_1.sequencer();
+    pedal_2.sequencer();
+    pedal_3.sequencer();
+    unusedPedal.sequencer();
+    sustainPedal.sequencer();
     relay1.sequencer();
 
     if(millis() & 0x200)
